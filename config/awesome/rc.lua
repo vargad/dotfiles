04dc6e4d97f537b845e7e8434f02691dece7e633
Dániel Varga <vargad88@gmail.com>
2018-07-07 17:32:49 +0200

    Simpler menu open with the new version

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -162,16 +162,7 @@ mytasklist.buttons = awful.util.table.join(
                                                   c:raise()
                                               end
                                           end),
-                     awful.button({ }, 3, function ()
-                                              if instance then
-                                                  instance:hide()
-                                                  instance = nil
-                                              else
-                                                  instance = awful.menu.clients({
-                                                      theme = { width = 250 }
-                                                  })
-                                              end
-                                          end),
+                     awful.button({ }, 3, client_menu_toggle_fn()),
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
                                               if client.focus then client.focus:raise() end
