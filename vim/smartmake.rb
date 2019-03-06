354dcf1b568f5ef347ddd1e12bd59fddb16e1fea
Dániel Varga <vargad88@gmail.com>
2019-03-06 22:27:00 +0100

    Smartmake to support Rakefile

--- vim/smartmake.rb
+++ vim/smartmake.rb
@@ -15,6 +15,9 @@ def find_project_root(dir)
         has_cmakelists = true
         return dir if has_build_dir && File.read(dir+"/CMakeLists.txt") =~ /project([^)]+)/
     end
+    if File.file?(dir+"/Rakefile")
+        return dir
+    end
     parent = File.dirname(dir)
     root = find_project_root(parent)
     return dir if root.nil? && has_cmakelists && has_build_dir
@@ -32,12 +35,6 @@ if PROJECT_ROOT.nil?
         exit 0
     end
 
-    if File.exists? 'Rakefile'
-        pid = spawn("rake")
-        Process.wait2 pid
-        exit 0
-    end
-
     file_content = File.read(CURRENT_FILE)
     if file_content.include?("#include <avr/") || file_content.include?("#include <avrcpp")
         system("/home/dev/arduino/compile.sh", CURRENT_FILE)
@@ -51,12 +48,21 @@ end
 puts "Args: #{$*}"
 
 puts "Project: #{PROJECT_ROOT}"
-pid = spawn("cd #{(PROJECT_ROOT+'/build').shellescape} && cmake .. && make -j#{CORES}")
-pid, result = Process.wait2 pid
-if result.exitstatus == 0
-    has_cppcheck_target = File.read("#{PROJECT_ROOT}/CMakeLists.txt").include? "add_custom_target(cppcheck"
-    if has_cppcheck_target
-        pid = spawn("cd #{(PROJECT_ROOT+'/build').shellescape} && make cppcheck")
-        Process.wait2 pid
+
+if File.file? PROJECT_ROOT+"/CMakeLists.txt"
+    pid = spawn("cd #{(PROJECT_ROOT+'/build').shellescape} && cmake .. && make -j#{CORES}")
+    pid, result = Process.wait2 pid
+    if result.exitstatus == 0
+        has_cppcheck_target = File.read("#{PROJECT_ROOT}/CMakeLists.txt").include? "add_custom_target(cppcheck"
+        if has_cppcheck_target
+            pid = spawn("cd #{(PROJECT_ROOT+'/build').shellescape} && make cppcheck")
+            Process.wait2 pid
+        end
     end
+elsif File.file? PROJECT_ROOT+"/Rakefile"
+    pid = spawn("rake --verbose")
+    Process.wait2 pid
+else
+    puts "Unknown build system"
+    exit 1
 end
