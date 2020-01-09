d80d80192972f1a90b10a43b2a4f4c88fd66f88c
Dániel Varga <vargad88@gmail.com>
2020-01-09 20:56:13 +0100

    Add some more common ports and pins

--- vim/avr/compat.h
+++ vim/avr/compat.h
@@ -9,12 +9,21 @@
 inline void _delay_ms(int ms) { usleep(ms*1000); }
 inline void _delay_us(int us) { usleep(us); }
 
+extern unsigned char UCSR0A;
 extern unsigned char UCSR0B;
+extern unsigned char UCSR0C;
+extern unsigned char UBRR0H;
+extern unsigned char UBRR0L;
 extern unsigned char DDRB;
 extern unsigned char DDRD;
 extern unsigned char PINB;
 extern unsigned char PIND;
 extern unsigned char PORTB;
 extern unsigned char PORTD;
+extern unsigned char UDR0;
+
+#define U2X0 1
+#define RXEN0 4
+#define TXEN0 3
 
 #endif
